79c5d7c483711397631d7d25f4303af6ab1a13cd
Rei Zhu <reizhu@hotmail.com>
2025-06-03 20:44:18 -0700

    Updates
    Major Changes:
    - Objects turn black when broken
    - Restart button added
    - Score counter added
    - Now there are two attacks: Nudge and Shove. Nudge is light push, shove sends objects flying
    
    - Added GameMaster object and GameMaster script to keep track of score and handle button click.

--- Assets/Scripts/SoundScript.cs
+++ Assets/Scripts/SoundScript.cs
@@ -1,31 +1,44 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class SoundScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Audio
     [SerializeField] private AudioClip bumpSound;
     [SerializeField] private AudioClip breakSound;
-    [Range(10f, 100f)]
-    public float dollarValue=50;
+    
+
+    // How much to darken when die
+    //public float darkenAmount = 0.5f; // 1 = original brightness, 0 = black
+
+    // Object State
+    private bool alive;
+    public float dollarValue;
     [Range(0, 10f)]
     public float breakVelocity = 2;
 
-    private bool alive;
+    Renderer renderer;
+    Color ogColor;
+    public Vector3 pos;
+    public Quaternion rot;
 
     private AudioSource audioSource;
     void Start()
     {
+        pos = transform.position;
+        rot = transform.rotation;
         audioSource = GetComponent<AudioSource>();
         alive = true;
+        renderer = GetComponent<Renderer>();
+        ogColor = renderer.material.color;
     }
      
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player" && alive)
         {
-            audioSource.clip = bumpSound;
-            audioSource.Play();
+            PlayNudge();
         }
 
         else
@@ -35,6 +48,9 @@ public class SoundScript : MonoBehaviour
                 audioSource.clip = breakSound;
                 audioSource.Play();
                 alive = false;
+                gameMaster.Instance.score += dollarValue;
+                gameMaster.Instance.scoreText.text = "Damage Done: $" + gameMaster.Instance.score;
+                Die();
             }
         }
     }
@@ -46,6 +62,9 @@ public class SoundScript : MonoBehaviour
             audioSource.clip = breakSound;
             audioSource.Play();
             alive = false;
+            gameMaster.Instance.score += dollarValue;
+            gameMaster.Instance.scoreText.text = "Damage Done: $" + gameMaster.Instance.score;
+            Die();
         }
     }
 
@@ -54,4 +73,37 @@ public class SoundScript : MonoBehaviour
     {
         
     }
+
+    public void PlayNudge()
+    {
+        audioSource.clip = bumpSound;
+        audioSource.Play();
+    }
+
+    public void Reset()
+    {
+        transform.position = pos;
+        transform.rotation = rot;
+        alive = true;
+        Live();
+    }
+    
+    void Die()
+    {
+        if (renderer != null)
+        {
+            renderer.material.color = ogColor * 0.25f;
+        }
+    }
+
+    void Live()
+    {
+        if (renderer != null)
+        {
+            renderer.material.color = ogColor;
+        }
+    }
 }
+
+
+    
---
+++ Assets/Scripts/gameMaster.cs
@@ -0,0 +1,67 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameMaster : MonoBehaviour
+{
+    public static gameMaster Instance { get; private set; }
+    // Score and Text
+    public TextMeshProUGUI scoreText;
+    public float score;
+    public Button restartButton;
+
+    // Objects
+    public Transform destructibles;
+    public Transform player;
+
+    void Awake()
+    {
+        // Singleton setup
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Persist across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Avoid duplicates
+        }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Damage Done: $" + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Reset()
+    {
+        foreach (Transform child in destructibles)
+        {
+            GameObject c = child.gameObject;
+            Rigidbody rb = c.GetComponent<Rigidbody>();
+            rb.linearVelocity = new Vector3 (0, 0, 0);
+            SoundScript objScript = c.GetComponent<SoundScript>();
+            if (objScript != null) { 
+                objScript.Reset();
+            }
+
+        }
+        Rigidbody playerRB = player.GetComponent<Rigidbody>();
+        playerRB.linearVelocity = new Vector3(0, 0, 0);
+        player.position = new Vector3(-1, 0, 7);
+        
+        score = 0;
+        scoreText.text = "Damage Done: $" + score;
+    }
+}
+ 
